44eb34cd336a09baf1231c613cc924b2a9ee0688
Paulo Roberto Torres <paulo.torres.apps@gmail.com>
2025-09-24 22:42:21 -0300

    feat: Remove dependência do godotenv e adiciona arquivo de credenciais exemplo

---
+++ internal/config/credentials.exemple.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	STRAVA_CLIENT_ID      = "123456"
+	STRAVA_CLIENT_SECRET  = "ijw49gjw98gw48v7wh408v7w54h0v87hw4g8"
+	API_KEY_THUNDERFOREST = "feoig90745yg9587h458v7h5480v5h4"
+	API_KEY_STADIAMAPS    = "aeg4geevverv-rvw-4e1f-be4averved-4erv4ver"
+)
